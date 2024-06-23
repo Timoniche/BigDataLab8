7a434b500e1828c4e92b8c16fc8fdc2a640b0d6d
timoniche <dmitriidulaev@gmail.com>
2024-06-24 01:02:47 +0300

    HDFS_CONF_dfs_client_use_datanode_hostname=true removed, hostname: datanode left only

--- k8s/lab6/build_all.sh
+++ k8s/lab6/build_all.sh
@@ -1,4 +1,9 @@
 kubectl apply -f namenode-service-lab6.yml
 kubectl apply -f namenode-deployment-lab6.yml
+
+sleep 20
+
 kubectl apply -f datanode-service-lab6.yml
 kubectl apply -f datanode-deployment-lab6.yml
+
+#kubectl apply -f spark-app-lab6.yml
--- k8s/lab6/clear_all.sh
+++ k8s/lab6/clear_all.sh
@@ -1,4 +1,7 @@
 kubectl delete service datanode
 kubectl delete deployment datanode
+
 kubectl delete service namenode
 kubectl delete deployment namenode
+
+kubectl delete sparkapplication kmeans-lab6
