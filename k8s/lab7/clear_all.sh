860636ed1738e4095f32573af00805966924bae0
timoniche <dmitriidulaev@gmail.com>
2024-06-25 03:43:58 +0300

    lab7: datamart deployment & service configs, images are in the hub

--- k8s/lab7/clear_all.sh
+++ k8s/lab7/clear_all.sh
@@ -4,4 +4,7 @@ kubectl delete deployment datanode
 kubectl delete service namenode
 kubectl delete deployment namenode
 
+kubectl delete service datamart
+kubectl delete deployment datamart
+
 kubectl delete sparkapplication kmeans-lab7
